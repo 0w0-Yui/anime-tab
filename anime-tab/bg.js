dedd19b74a837da747e8520c73f419376f6b0c91
Yui <yui@lioat.cn>
2022-03-30 20:32:50 -0700

    Delete anime-tab directory

--- anime-tab/bg.js
+++
@@ -1,8 +0,0 @@
-
-if (localStorage.getItem("bg_url") == null) {
-    localStorage.setItem("bg_url", "http://www.dmoe.cc/random.php");
-    bg_url = localStorage.getItem("bg_url");
-} else {
-    bg_url = localStorage.getItem("bg_url");
-}
-document.body.style.backgroundImage = "url('" + bg_url + "')";
