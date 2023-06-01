a836166372ceb5a74e4957040d7476b5a1f9fe7d
Yui <yui@lioat.cn>
2023-06-01 15:29:14 +0800

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,8 +1,11 @@
 
 if (localStorage.getItem("bg_url") == null) {
-    localStorage.setItem("bg_url", "http://www.dmoe.cc/random.php");
+    localStorage.setItem("bg_url", "https://www.loliapi.com/acg/pc/");
     bg_url = localStorage.getItem("bg_url");
 } else {
     bg_url = localStorage.getItem("bg_url");
 }
+var screen_width = screen.width;
+console.debug(screen_width);
+document.body.style.backgroundSize = String(screen_width) + "px";
 document.body.style.backgroundImage = "url('" + bg_url + "')";
